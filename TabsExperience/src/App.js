2f1aa584c8c035aafc15fac6708a1d43bcd0b40a
Rairu <lylekevin.tamarra.21@usjr.edu.ph>
2022-11-03 18:51:45 +0800

    src files

--- TabsExperience/src/App.js
+++ TabsExperience/src/App.js
@@ -1,22 +1,47 @@
-import React, { useState } from 'react'
-import data from './data'
-import SingleQuestion from './Question'
+import "./styles.css";
+import React, { useState } from "react";
+import Experience from "./data";
 
-const App = () => {
-  const [questions, setQuestions] = useState(data)
+export default function App() {
+  const [nav, chooseNav] = useState(0);
+
+  const section = Experience.map((prop) => {
+    return (
+      <section className="profile">
+        <h2>{prop.role}</h2>
+        <h3>{prop.name}</h3>
+        <h4>{prop.date}</h4>
+        <ul style={{ paddingLeft: 15 }}>
+          {prop.description.map((desc) => (
+            <li className="description">{desc}</li>
+          ))}
+        </ul>
+      </section>
+    );
+  });
 
   return (
-    <main>
-      <div className='container'>
-        <h3>questions and answers about login</h3>
-        <section className='info'>
-          {questions.map((question) => (
-            <SingleQuestion key={question.id} {...question} />
-          ))}
-        </section>
-      </div>
-    </main>
-  )
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>Experience</h1>
+      <section className="navDesc">
+        <nav>
+          <ul>
+            <li className="navButton">
+              <button onClick={() => chooseNav(0)}>TOMMY</button>
+            </li>
+            <li className="navButton">
+              <button onClick={() => chooseNav(1)}>BIGDROP</button>
+            </li>
+            <li className="navButton">
+              <button onClick={() => chooseNav(2)}>CUKER</button>
+            </li>
+          </ul>
+        </nav>
+        {section[nav]}
+      </section>
+      <button onClick={() => {}} className="moreInfo">
+        MORE INFO
+      </button>
+    </div>
+  );
 }
-
-export default App
--- TabsExperience/src/data.js
+++ TabsExperience/src/data.js
@@ -1,33 +1,35 @@
-const questions = [
+const Experience = [
   {
-    id: 1,
-    title: 'Do I have to allow the use of cookies?',
-    info:
-      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+    role: "Full Stack Web Developer",
+    name: "TOMMY",
+    date: "Decemeber 2015 - Present",
+    description: [
+      "Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge disrupt.",
+      "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
+      "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian."
+    ]
   },
   {
-    id: 2,
-    title: 'How do I change my My Page password?',
-    info:
-      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+    role: "Front-End Engineer",
+    name: "BIGDROP",
+    date: "May 2015 - December 2015",
+    description: [
+      "Hashtag drinking vinegar scenester mumblecore snackwave four dollar toast, lumbersexual XOXO. Cardigan church-key pabst, biodiesel vexillologist viral squid.",
+      "Franzen af pitchfork, mumblecore try-hard kogi XOXO roof party la croix cardigan neutra retro tattooed copper mug. Meditation lomo biodiesel scenester",
+      "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
+      "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1"
+    ]
   },
   {
-    id: 3,
-    title: 'What is BankID?',
-    info:
-      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
-  },
-  {
-    id: 4,
-    title: 'Whose birth number can I use?',
-    info:
-      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
-  },
-  {
-    id: 5,
-    title: 'When do I recieve a password ordered by letter?',
-    info:
-      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
-  },
-]
-export default questions
+    role: "Engineering Intern",
+    name: "CUKER",
+    date: "May 2014 - September 2015",
+    description: [
+      "I'm baby woke mumblecore stumptown enamel pin. Snackwave prism pork belly, blog vape four loko sriracha messenger bag jean shorts DIY bushwick VHS. Banjo post-ironic hella af, palo santo craft beer gluten-free.",
+      "YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.",
+      "Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism."
+    ]
+  }
+];
+
+export default Experience;
