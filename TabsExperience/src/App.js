58304982ca1f562f42d593ec25f386ccf6424d29
Rairu <lylekevin.tamarra.21@usjr.edu.ph>
2022-11-03 22:22:23 +0800

    edit tabs css

--- TabsExperience/src/App.js
+++ TabsExperience/src/App.js
@@ -1,46 +1,71 @@
-import "./styles.css";
-import React, { useState } from "react";
-import Experience from "./data";
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
 
-export default function App() {
-  const [nav, chooseNav] = useState(0);
+const url = 'https://course-api.com/react-tabs-project'
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
 
-  const section = Experience.map((prop) => {
+  const fetchJobs = async () => {
+    const reponse = await fetch(url)
+    const newJobs = await reponse.json()
+
+    setJobs(newJobs)
+    setLoading(false)
+  }
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+  if (loading) {
     return (
-      <article className="job-info">
-        <h3>{prop.role}</h3>
-        <h4>{prop.name}</h4>
-        <p className="job-date">{prop.date}</p>
-        <div>
-          {prop.description.map((desc) => (
-            <div className="job-desc">{desc}</div>
-          ))}
-        </div>
-      </article>
-    );
-  });
-
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+  const { company, dates, duties, title } = jobs[value]
   return (
-    <body>
-    <div className="App">
-      <div className="title">
-        <h2>Experience</h2>
-        <div class="underline"></div>
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
       </div>
-      
-      <div className="jobs-center">
-        <div className="btn-center">
-          <button className="job-btn" onClick={() => chooseNav(0)}>TOMMY</button>
-          <button className="job-btn" onClick={() => chooseNav(1)}>BIGDROP</button>
-          <button className="job-btn" onClick={() => chooseNav(2)}>CUKER</button>
+      <div className='jobs-center'>
+        {/* btn container */}
+        <div className='btn-container'>
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
         </div>
-        {section[nav]}
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
       </div>
-
-      <button onClick={() => {}} className="btn">
-        MORE INFO
+      <button type='button' className='btn'>
+        more info
       </button>
-    </div>
-    </body>
-  );
+    </section>
+  )
 }
+
+export default App
