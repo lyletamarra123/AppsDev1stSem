36ed851c41b4b921f0509a38e1d5fbcd4348d290
Rairu <lylekevin.tamarra.21@usjr.edu.ph>
2022-12-08 21:12:15 +0800

    finalized

--- ClothingShopVite/src/components/Cart/Cart.tsx
+++ ClothingShopVite/src/components/Cart/Cart.tsx
@@ -1,10 +1,19 @@
-import { ProductsWrapper, Title } from './Cart.styled';
+import { useContext } from "react";
+import { ClothingShopContext, } from "../../useContext/shopContext";
+import { Product } from "../../models";
+import { ProductCard } from "../ProductCard";
+import { ProductsWrapper, Title } from "./Cart.styled";
 
 export const Cart = () => {
+  const { products, total } = useContext(ClothingShopContext);
   return (
     <>
-      <Title>Your cart total is 00.00$</Title>
-      <ProductsWrapper></ProductsWrapper>
+      <Title>Your cart total is {total}.00$</Title>
+      <ProductsWrapper>
+        {products.map((product: Product, index) => (
+          <ProductCard {...product} key={index} />
+        ))}
+      </ProductsWrapper>
     </>
   );
 };
--- ClothingShopVite/src/components/ProductCard/ProductCard.tsx
+++ ClothingShopVite/src/components/ProductCard/ProductCard.tsx
@@ -1,18 +1,37 @@
-import {
-  AddButton,
-  SubTitle,
-  TextContainer,
-  Title,
-  Wrapper,
-} from './ProductCard.styled';
+import { AddButton, SubTitle,TextContainer,Title,Wrapper,} from './ProductCard.styled';
 
+import { useState, useEffect, useContext } from 'react';
 import { Product } from '../../models';
+import { ClothingShopContext} from '../../useContext';
 
 export const ProductCard = ({ name, imageUrl, price }: Product) => {
+  const {products, addToCart, removeItem} = useContext(ClothingShopContext);
+  const [isInCart, setIsInCart] = useState(false);
+  
+  useEffect(() => {
+    const itemInCart = products.find((product: { name: string; }) => product.name === name);
+
+    if (itemInCart) {
+      setIsInCart(true);
+    } else {
+      setIsInCart(false);
+    }
+  }, [products, name]);
+  
+  const handleClick = () => {
+    const product = {name, imageUrl, price};
+    if(isInCart){
+      removeItem(product);
+      setIsInCart(false);
+    } else{
+      addToCart(product);
+      setIsInCart(true);
+    }
+  }
   return (
     <Wrapper background={imageUrl}>
-      <AddButton isInCart={false} onClick={() => console.log('Implement Me')}>
-        <p>+</p>
+      <AddButton isInCart={isInCart} onClick={handleClick}>
+        <p>{isInCart? "-" : "+"}</p>
       </AddButton>
       <TextContainer>
         <Title>{name}</Title>
